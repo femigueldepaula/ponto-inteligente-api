8ffb3d08cac8739695f017c853ac0096dccd2b79
Felipe De Paula <femigueldepaula@gmail.com>
2018-03-17 00:23:09 -0300

    Lancamento Controller - criacao

--- src/main/java/com/kazale/inteligente/api/controllers/FuncionarioController.java
+++ src/main/java/com/kazale/inteligente/api/controllers/FuncionarioController.java
@@ -1,28 +1,77 @@
 package com.kazale.inteligente.api.controllers;
 
+import java.math.BigDecimal;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kazale.inteligente.api.dtos.FuncionarioDto;
 import com.kazale.inteligente.api.entities.Funcionario;
 import com.kazale.inteligente.api.response.Response;
 import com.kazale.inteligente.api.services.FuncionarioService;
+import com.kazale.inteligente.api.utils.PasswordUtils;
 
 @RestController
 @RequestMapping("/api/funcionarios")
 public class FuncionarioController {
 
+	private static final Logger log = LoggerFactory.getLogger(EmpresaController.class);
+	
 	@Autowired
 	private FuncionarioService funcionarioService;
 
-	
+
+	/**
+	 * Atualiza os dados de um funcionário.
+	 * 
+	 * @param id
+	 * @param funcionarioDto
+	 * @param result
+	 * @return ResponseEntity<Response<FuncionarioDto>>
+	 * @throws NoSuchAlgorithmException
+	 */
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Response<FuncionarioDto>> atualizar(@PathVariable("id") Long id,
+			@Valid @RequestBody FuncionarioDto funcionarioDto, BindingResult result) throws NoSuchAlgorithmException {
+		
+		log.info("Atualizando Funcionário: {}", funcionarioDto.toString());
+		
+		Response<FuncionarioDto> response = new Response<FuncionarioDto>();
+		
+		Optional<Funcionario> funcionario = this.funcionarioService.buscaPorId(id);
+		if (!funcionario.isPresent()) {
+			result.addError(new ObjectError("funcionario", "Funcionario não encontrado"));
+		}
+		
+		this.atualizarDadosFuncionario(funcionario.get(), funcionarioDto, result);
+		
+		if (result.hasErrors()) {
+			log.error("Erro validando funcionário: {}", result.getAllErrors());
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		this.funcionarioService.persistir(funcionario.get());
+		response.setData(this.convertFuncionarioDto(funcionario.get()));
+		return ResponseEntity.ok(response);
+	}
+
 	/**
 	 * Retrona um funcionario por CPF
 	 * 
@@ -82,6 +131,7 @@ public class FuncionarioController {
 
 		if (funcionarios == null || funcionarios.isEmpty()) {
 			response.getErrors().add("Não existem funcionários cadastrados.");
+			return ResponseEntity.badRequest().body(response);
 		}
 
 		response.setData(this.convertFuncionariosDto(funcionarios));
@@ -142,5 +192,39 @@ public class FuncionarioController {
 		}
 		return funcionariosDto;
 
+	}
+	
+	/**
+	 * Atualiza os dados do funcionário com base nos dados encontrados no DTO.
+	 * 
+	 * @param funcionario
+	 * @param funcionarioDto
+	 * @param result
+	 * @throws NoSuchAlgorithmException
+	 */
+	private void atualizarDadosFuncionario(Funcionario funcionario, FuncionarioDto funcionarioDto, BindingResult result)
+			throws NoSuchAlgorithmException {
+		funcionario.setNome(funcionarioDto.getNome());
+
+		if (!funcionario.getEmail().equals(funcionarioDto.getEmail())) {
+			this.funcionarioService.buscaPorEmail(funcionarioDto.getEmail())
+					.ifPresent(func -> result.addError(new ObjectError("email", "Email já existente.")));
+			funcionario.setEmail(funcionarioDto.getEmail());
+		}
+
+		funcionario.setQtdHorasAlmoco(null);
+		funcionarioDto.getQtdHorasAlmoco()
+				.ifPresent(qtdHorasAlmoco -> funcionario.setQtdHorasAlmoco(Float.valueOf(qtdHorasAlmoco)));
+
+		funcionario.setQtdHorasTrabalhoDia(null);
+		funcionarioDto.getQtdHorasTrabalhoDia()
+				.ifPresent(qtdHorasTrabDia -> funcionario.setQtdHorasTrabalhoDia(Float.valueOf(qtdHorasTrabDia)));
+
+		funcionario.setValorHora(null);
+		funcionarioDto.getValorHora().ifPresent(valorHora -> funcionario.setValorHora(new BigDecimal(valorHora)));
+
+		if (funcionarioDto.getSenha().isPresent()) {
+			funcionario.setSenha(PasswordUtils.gerarBCrypt(funcionarioDto.getSenha().get()));
+		}
 	}
 }
