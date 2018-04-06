9f3ddf3af6ce370eb32ec4db03e2382ce5cedb4d
Felipe De Paula <femigueldepaula@gmail.com>
2018-04-05 23:57:58 -0300

    Implementacao JWT

--- src/main/java/com/kazale/inteligente/api/controllers/LancamentoController.java
+++ src/main/java/com/kazale/inteligente/api/controllers/LancamentoController.java
@@ -164,7 +164,7 @@ public class LancamentoController {
 	 * @return ResponseEntity<Response<Lancamento>>
 	 */
 	@DeleteMapping(value = "/{id}")
-	/*@PreAuthorize("hasAnyRole('ADMIN')")*/
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	public ResponseEntity<Response<String>> remover(@PathVariable("id") Long id) {
 		log.info("Removendo lançamento: {}", id);
 		Response<String> response = new Response<String>();
---
+++ src/main/java/com/kazale/inteligente/api/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.kazale.inteligente.api.security.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.kazale.inteligente.api.entities.Funcionario;
+import com.kazale.inteligente.api.security.JwtUserFactory;
+import com.kazale.inteligente.api.services.FuncionarioService;
+
+
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscaPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
